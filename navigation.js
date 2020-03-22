46d565ddbf0bb6d30c682884e75a7fdb96a92725
NicellaO <57523487+NicellaO@users.noreply.github.com>
2020-03-22 23:37:18 +0100

    Sista :( 22/3

--- navigation.js
+++ navigation.js
@@ -1,28 +1,46 @@
-(function() {
-  
-    'use strict';
-     
-    const mySwiper = new Swiper ('#swiper1', {
-      
-      loop: true, 
-      
-        slidesPerView: 'auto',
-        centeredSlides: true,
-      
-      a11y: true,
-      keyboardControl: true,
-      grabCursor: true,
-      
-      // pagination
-      pagination: '.swiper-pagination',
-      paginationClickable: true,
-      
-      // navigation arrows
-      nextButton: '#js-prev1',
-      prevButton: '#js-next1',
-      
-    });
-  
-    
-  
-  })(); /* IIFE end */
+
+
+$('.responsive').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+{
+  breakpoint: 1024,
+  settings: {
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    infinite: true,
+    dots: true
+  }
+},
+{
+  breakpoint: 600,
+  settings: {
+    slidesToShow: 2,
+    slidesToScroll: 2
+  }
+},
+{
+  breakpoint: 480,
+  settings: {
+    slidesToShow: 1,
+    slidesToScroll: 1
+  }
+}
+// You can unslick at a given breakpoint now by adding:
+// settings: "unslick"
+// instead of a settings object
+]
+});
